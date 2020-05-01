cd4544704f32a9f6b892cc74485b7cba8e57a545
xuanruixiang <xuanruixiang@shgbit.com>
2020-05-02 06:34:28 +0800

    修改代码风格

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -5,13 +5,9 @@ import java.io.File;
 import java.io.FileReader;
 
 public class AppRunner {
-
+    static TaxiFare taxiFare = new TaxiFare();
+    static CommonUtil commonUtil = new CommonUtil();
     public static void main(String[] args) throws Exception {
-        /*TODO
-          1. args[0]为resources下的测试数据文件名，例如传入的args[0]值为"testData.txt"，注意并不包含文件路径。
-          2. 你写的程序将把testDataFile作为参数加载此文件并读取文件内的测试数据，并对每条测试数据计算结果。
-          3. 将所有计费结果拼接并使用\n分割，然后保存到receipt变量中。
-         */
         String testDataFile = args[0];
         String fileName = "src/main/resources/" + testDataFile;
         String receipt = getFareByTxt(fileName);
@@ -19,8 +15,6 @@ public class AppRunner {
     }
 
     public static String getFareByTxt(String fileName) throws Exception {
-        TaxiFare taxiFare = new TaxiFare();
-        CommonUtil commonUtil = new CommonUtil();
         BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
         String inputLine;
         String receipt = "";
--- src/main/java/com/jiker/keju/TaxiFare.java
+++ src/main/java/com/jiker/keju/TaxiFare.java
@@ -2,13 +2,7 @@ package com.jiker.keju;
 
 
 public class TaxiFare {
-    /**
-     * 计算总价
-     *
-     * @param miles
-     * @param waitingMinutes
-     * @return
-     */
+
     public int calculationFare(int miles, int waitingMinutes) {
         double fareOfStarting = calculationFareStarting();
         double fareOfWaiting = calculationFareOfWaiting(waitingMinutes);
@@ -19,43 +13,24 @@ public class TaxiFare {
         return actualFare;
     }
 
-    /**
-     * 等待费
-     *
-     * @param waitingMinutes
-     * @return
-     */
+
     private double calculationFareOfWaiting(int waitingMinutes) {
         return FareStrategyEnum.FARE_PER_MINUTES.getRatio() * waitingMinutes;
     }
 
-    /**
-     * 超过2公里的费用
-     *
-     * @param miles
-     * @return
-     */
+
     private double calculationFareOverTwo(int miles) {
         double milesOverTwo = miles - FareStrategyEnum.MILES_STARTING.getRatio() > 0 ? miles - FareStrategyEnum.MILES_STARTING.getRatio() : 0;
         return milesOverTwo * FareStrategyEnum.FARE_LARGER_THAN_TWO_KILOMETER.getRatio();
     }
 
-    /**
-     * 计算长途费
-     *
-     * @param miles
-     * @return
-     */
+
     private double calculationFareTooLong(int miles) {
         double milesOverEight = miles - FareStrategyEnum.MILES_TOO_LONG.getRatio() > 0 ? miles - FareStrategyEnum.MILES_TOO_LONG.getRatio() : 0;
         return milesOverEight * FareStrategyEnum.FARE_TOO_LONG.getRatio() * FareStrategyEnum.FARE_LARGER_THAN_TWO_KILOMETER.getRatio();
     }
 
-    /**
-     * 获取起步费
-     *
-     * @return
-     */
+
     private double calculationFareStarting() {
         return FareStrategyEnum.FARE_STARTING.getRatio();
     }
